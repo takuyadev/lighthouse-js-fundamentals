f40cd5539502009703ebb7a61d806a61505f03d3
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-02-17 15:27:58 -0800

    added packing list

---
+++ packing-list.js
@@ -0,0 +1,16 @@
+const packingList = [
+  "bowls",
+  "plates",
+  "pots",
+  "pans",
+  "eating utensils",
+  "glasses",
+  "cups",
+  "cooking utensils",
+]
+
+console.log("Kitchen stuff to pack:")
+
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i])
+}
